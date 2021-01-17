6a6489e2b1e1740fb3352f49a446c890baa380c8
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-01-17 01:02:19 -0800

    Initial Commit

---
+++ Audio Recorder/Audio_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_RecorderApp.swift
+//  Audio Recorder
+//
+//  Created by Shengyuan Lu on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Audio_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
